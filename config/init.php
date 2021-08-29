9124866997445c34c5e15330b19f9f4759d59430
subhadipnandi1991 <subha.nandi2014@gmail.com>
2021-08-29 11:59:45 +0530

    Creating job done. Now anyone can add jobs with all its information and 's gonna shown at the front page.

--- config/init.php
+++ config/init.php
@@ -1,6 +1,13 @@
 <?php
+// Session start
+session_start();
+
 // Config File
 require_once 'config.php';
+
+// Helpers File
+require_once 'helpers/system_helper.php';
+
 
 // Autoloader
 function __autoload($class_name){
--- lib/Job.php
+++ lib/Job.php
@@ -62,4 +62,25 @@
 			return $result;
 
 		}
+
+		public function create($data)
+		{
+			$this->db->query("INSERT INTO jobs (category_id, company, job_title, description, salary, location, contact_user, contact_email) VALUES (:category_id, :company, :job_title, :description, :salary, :location, :contact_user, :contact_email)");
+			$this->db->bind(':category_id', $data['category_id']);
+			$this->db->bind(':company', $data['company']);
+			$this->db->bind(':job_title', $data['job_title']);
+			$this->db->bind(':description', $data['description']);
+			$this->db->bind(':salary', $data['salary']);
+			$this->db->bind(':location', $data['location']);
+			$this->db->bind(':contact_user', $data['contact_user']);
+			$this->db->bind(':contact_email', $data['contact_email']);
+
+			if ($this->db->execute()) {
+				return true;
+			} else {
+				return false;
+			}
+
+
+		}
 	}
